b8c0d98d455c9ec653084c06e900a57802a7f31b
miauware <miauware@outlook.com>
2026-03-17 14:46:46 -0300

    🐛 fix: fix treesitter error and  configuration migrated to plugin
    Resolved error when updating via lazy sync and configuration migrated from init.lua to nvim-treesitter.lua
    
    Changelog: fix

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,8 +1,75 @@
-return{
-  -- INFO: Treesitter configurations 
-    'nvim-treesitter/nvim-treesitter',
+return {
+  'nvim-treesitter/nvim-treesitter',
+  lazy = false,
+  build = ':TSUpdate',
     dependencies = {
-      'nvim-treesitter/nvim-treesitter-textobjects',
-    },
-    build = ':TSUpdate',
-  }
+    'nvim-treesitter/nvim-treesitter-textobjects',
+  },
+  config = function()
+    -- INFO: load treesitter safely after plugin is ready
+    local ok, configs = pcall(require, 'nvim-treesitter.configs')
+    if not ok then
+      return
+    end
+
+    configs.setup({
+      ensure_installed = {
+        'c','css','cpp','go','markdown','lua',
+        'python','rust','tsx','typescript','vimdoc','vim'
+      },
+      auto_install = false,
+      highlight = { enable = true },
+      indent = { enable = true },
+
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = '<c-space>',
+          node_incremental = '<c-space>',
+          scope_incremental = '<c-s>',
+          node_decremental = '<M-space>',
+        },
+      },
+
+      textobjects = {
+        select = {
+          enable = true,
+          lookahead = true,
+          keymaps = {
+            ['aa'] = '@parameter.outer',
+            ['ia'] = '@parameter.inner',
+            ['af'] = '@function.outer',
+            ['if'] = '@function.inner',
+            ['ac'] = '@class.outer',
+            ['ic'] = '@class.inner',
+          },
+        },
+        move = {
+          enable = true,
+          set_jumps = true,
+          goto_next_start = {
+            [']m'] = '@function.outer',
+            [']]'] = '@class.outer',
+          },
+          goto_next_end = {
+            [']M'] = '@function.outer',
+            [']['] = '@class.outer',
+          },
+          goto_previous_start = {
+            ['[m'] = '@function.outer',
+            ['[['] = '@class.outer',
+          },
+          goto_previous_end = {
+            ['[M'] = '@function.outer',
+            ['[]'] = '@class.outer',
+          },
+        },
+        swap = {
+          enable = true,
+          swap_next = { ['<leader>a'] = '@parameter.inner' },
+          swap_previous = { ['<leader>A'] = '@parameter.inner' },
+        },
+      },
+    })
+  end
+}
