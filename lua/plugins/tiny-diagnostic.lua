c3a50183b43a84d2a0a1fd3612820a06c6ba2029
miauware <miauware@outlook.com>
2026-03-30 13:35:37 -0300

    🌟 feat: added tiny diagnostic plugin
    Changelog: feature

---
+++ lua/plugins/tiny-diagnostic.lua
@@ -0,0 +1,14 @@
+return {
+    "rachartier/tiny-inline-diagnostic.nvim",
+    event = "VeryLazy",
+    priority = 1000,
+    config = function()
+        require("tiny-inline-diagnostic").setup({
+            options = {
+                multilines = {enabled = true, always_show = true},
+                enable_on_insert = true
+            }
+        })
+        vim.diagnostic.config({virtual_text = false})
+    end
+}
